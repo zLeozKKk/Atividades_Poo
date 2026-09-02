13b563faab3099c3ad93b0674e69a366e942d4ef
Leonardo Machado <leo.md@outlook.com>
2026-09-02 19:31:30 -0300

    Codigo dos tipos de pessoas 1.2

--- Pessoa.java
+++ Pessoa.java
@@ -1,20 +1,44 @@
 package Heranca;
 
-public class Pessoa {
+import java.util.List;
 
+public abstract class Pessoa {
+
+    private static int idInicial = 100; 
     private int id;
 
-    public Pessoa(int id) {
-        this.id = id;
+    public Pessoa() {
+        this.id = idInicial;
+        idInicial++;
     }
     
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public static Pessoa localizaPessoa (List<Pessoa> listaPessoas, int idProcurado){
+            for(Pessoa p : listaPessoas ){
+                if(p.getId() == idProcurado){
+                    return p;
+                }
+            }return null;
+        }
 
-    
+    public static Pessoa localizaPorDocumento(List<Pessoa> lista, String documento) {
+    for (Pessoa p : lista) {
+        if (p instanceof PessoaFisica) {
+            PessoaFisica pf = (PessoaFisica) p;
+            if (pf.getCpf().equals(documento)) {
+                return pf;
+            }
+        } 
+        else if (p instanceof PessoaJuridica) {
+            PessoaJuridica pj = (PessoaJuridica) p;
+            if (pj.getCnpj().equals(documento)) {
+                return pj;
+            }
+        }
+    }
+    return null; 
+}
 }
